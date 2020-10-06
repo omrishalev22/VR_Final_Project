2a778aea8feae5eec885a72ff6d2ca4815cf7be5
Omri Shalev <omshalev@microsoft.com>
2020-10-06 14:42:23 +0300

    Fixing clicks on menu

--- Assets/Scripts/UI/InputController.cs
+++ Assets/Scripts/UI/InputController.cs
@@ -6,50 +6,71 @@ using UnityEngine.SceneManagement;
 
 public class InputController : MonoBehaviour
 {
-    [SerializeField]
-    private Camera m_mainCam;
+    //[SerializeField]
+    //private Camera m_mainCam;
 
-    private Ray m_ray;
-    private RaycastHit m_hit;
-    private string m_hitted;
-    private bool m_hittedObj;
+    //private Ray m_ray;
+    //private RaycastHit m_hit;
+    //private string m_hitted;
+    //private bool m_hittedObj;
 
-    // Update is called once per frame
-    void Update()
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (Input.anyKeyDown)
+    //    {
+    //        m_ray = m_mainCam.ScreenPointToRay(new Vector3(Screen.width / 4f, Screen.height / 2f, 0f));
+    //        if (Physics.Raycast(m_ray, out m_hit))
+    //        {
+    //            if (m_hit.rigidbody != null)
+    //            {
+    //                m_hitted = m_hit.rigidbody.transform.name;
+    //                m_hittedObj = true;
+    //            }
+    //        }
+    //        if (m_hittedObj)
+    //        {
+    //            m_hittedObj = false;
+    //            switch (m_hitted)
+    //            {
+    //                case "PlayButton":
+    //                    GameManager.isGameOver = false;
+    //                    SceneManager.LoadScene(1);
+    //                    break;
+    //                case "ExitButton":
+    //                    Application.Quit();
+    //                    break;
+    //                case "ScoresButton":
+    //                    SceneManager.LoadScene(2);
+    //                    break;
+    //                case "MenuButton":
+    //                    SceneManager.LoadScene(0);
+    //                    break;
+    //                default:
+    //                    break;
+    //            }
+    //        }
+    //    }
+    //}
+
+    public void PlayGame()
     {
-        if (Input.anyKeyDown)
-        {
-            m_ray = m_mainCam.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
-            if (Physics.Raycast(m_ray, out m_hit))
-            {
-                if (m_hit.rigidbody != null)
-                {
-                    m_hitted = m_hit.rigidbody.transform.name;
-                    m_hittedObj = true;
-                }
-            }
-            if (m_hittedObj)
-            {
-                m_hittedObj = false;
-                switch (m_hitted)
-                {
-                    case "PlayButton":
-                        GameManager.isGameOver = false;
-                        SceneManager.LoadScene(1);
-                        break;
-                    case "ExitButton":
-                        Application.Quit();
-                        break;
-                    case "ScoresButton":
-                        SceneManager.LoadScene(2);
-                        break;
-                    case "MenuButton":
-                        SceneManager.LoadScene(0);
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
+        GameManager.isGameOver = false;
+        SceneManager.LoadScene(1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ScoresMenu()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ExitMenu()
+    {
+        Application.Quit();
     }
 }
