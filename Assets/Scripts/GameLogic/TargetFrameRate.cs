d4010b3213db601a72e9d9a163c4f90385744c1b
Omri Shalev <omshalev@microsoft.com>
2020-10-07 15:40:42 +0300

    last version

---
+++ Assets/Scripts/GameLogic/TargetFrameRate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFrameRate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 32;
+        
+    }
+
+}
